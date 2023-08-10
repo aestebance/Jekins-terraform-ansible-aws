94b5d1eaa1125aae89b0f001e2ce69dd87e2845d
Alejandro Esteban Celdran <aestebance@uoc.edu>
2023-08-10 23:35:32 +0200

    first commit

---
+++ security_groups.tf
@@ -0,0 +1,90 @@
+#Create SG for Load Balancer, only TCP/80 TCP/443 and outbound all
+resource "aws_security_group" "lb-sg" {
+  provider    = aws.region-master
+  name        = "lb-sg"
+  description = "Allow 443 and traffic to Jenkins SG"
+  vpc_id      = aws_vpc.vpc_master.id
+  ingress {
+    description = "Allow 443 from anywhere"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allow 80 from anywhere for redirection"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+#Create SG for allow TCP/8080 from * and TCP/22 from your IP in eu-west-1
+resource "aws_security_group" "jenkins-sg" {
+  provider    = aws.region-master
+  name        = "jenkins-sg"
+  description = "Allow TCP/8080 and TCP/22"
+  vpc_id      = aws_vpc.vpc_master.id
+  ingress {
+    description = "Allow 22 from our public IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.external_ip]
+  }
+  ingress {
+    description     = "Allow anyone on port 8080"
+    from_port       = 8080
+    to_port         = 8080
+    protocol        = "tcp"
+    security_groups = [aws_security_group.lb-sg.id]
+  }
+  ingress {
+    description = "Allow traffic from eu-west-2"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["192.168.0.0/16"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+#Create SG for allow TCP/22 from your ip in eu-west-2
+resource "aws_security_group" "jenkins-sg-paris" {
+  provider    = aws.region-worker
+  name        = "jenkins-sg-paris"
+  description = "Allow TCP/8080 and TC/22"
+  vpc_id      = aws_vpc.vpc_worker_paris.id
+  ingress {
+    description = "Allow 22 from our public IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.external_ip]
+  }
+  ingress {
+    description = "Allow trafic from eu-west-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
