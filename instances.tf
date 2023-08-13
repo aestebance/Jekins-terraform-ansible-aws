9c480097c1eabe1341fce7771bcf4bc1566ade41
Alejandro Esteban Celdran <aestebance@uoc.edu>
2023-08-13 16:19:11 +0200

    updated ansible playbooks

--- instances.tf
+++ instances.tf
@@ -45,9 +45,8 @@ resource "aws_instance" "jenkins-master" {
     command = <<EOF
 aws --profile ${var.profile} ec2 wait instance-status-ok --region ${var.region-master} --instance-ids ${self.id}
 ansible-playbook --extra-vars 'passed_in_hosts=tag_Name_${self.tags.Name}' ansible_templates/install_jenkins_master.yml
-echo ${self.private_ip} > jenkins_private_ip.txt
 EOF
-  }
+   }
 }
 
 #Create EC2 in eu-west-2
@@ -73,16 +72,16 @@ ansible-playbook --extra-vars 'passed_in_hosts=tag_Name_${self.tags.Name} master
 EOF
   }
 
-  provisioner "remote-exec" {
-    when = destroy
-    inline = [
-      "java -jar /home/ec2-user/jenkins-cli.jar -auth @/home/ec2-user/jenkins_auth -s http://$(cat jenkins_private_ip.txt):8080 delete-node ${self.private_ip}"
-    ]
-    connection {
-      type        = "ssh"
-      user        = "ec2-user"
-      private_key = file("~/.ssh/id_rsa")
-      host        = self.public_ip
-    }
-  }
+  # provisioner "remote-exec" {
+  #   when = destroy
+  #   inline = [
+  #     "java -jar /home/ec2-user/jenkins-cli.jar -auth @/home/ec2-user/jenkins_auth -s http://${cat /home/ec2-user/master_ip.txt}:8080 delete-node ${self.private_ip}"
+  #   ]
+  #   connection {
+  #     type        = "ssh"
+  #     user        = "ec2-user"
+  #     private_key = file("~/.ssh/id_rsa")
+  #     host        = self.public_ip
+  #   }
+  # }
 }
